e534c81940270a3ab4518dbc777bf3fe5fb02a30
scenaristeur@gmail.com <scenaristeur@gmail.com>
2019-11-25 13:58:12 +0100

    compagent initial

--- compagent-initial/components/component-modele.js
+++ compagent-initial/components/component-modele.js
@@ -1,16 +1,14 @@
-import { LitElement, css,  html } from '../vendor/lit-element/lit-element.min.js';
-//import { LitElement, css,  html } from 'https://cdn.pika.dev/lit-element/^2.2.1';
+import { LitElement, css,  html } from 'https://cdn.pika.dev/lit-element/^2.2.1';
 import { HelloAgent } from '../agents/HelloAgent.js';
 
 // Extend the LitElement base class
-class MessagesComponent extends LitElement {
+class ComponentModele extends LitElement {
 
   static get properties() {
     return {
       message: { type: String },
       name: {type: String},
-      count: {type: Number},
-      messages: {type: Array}
+      count: {type: Number}
     };
   }
 
@@ -18,20 +16,14 @@ class MessagesComponent extends LitElement {
     super();
     this.message = 'Hello world! From minimal-element';
     this.name = "unknown"
-    this.messages =  []
     this.count = 0;
-
+    
   }
 
   firstUpdated(changedProperties) {
     var app = this;
     this.agent = new HelloAgent(this.name);
-    console.log(this.agent)
     this.agent.receive = function(from, message) {
-      console.log(message)
-      app.messages.reverse()
-      app.messages = [... app.messages, {message: message, from: from}]
-      app.messages.reverse()
       if (message.hasOwnProperty("action")){
         switch(message.action) {
           case "doSomething":
@@ -48,24 +40,15 @@ class MessagesComponent extends LitElement {
 
   render() {
     return html`
-    <style>
-    .pre-scrollable {
-      max-height: 340px;
-      overflow-y: scroll;
-    }
-    </style>
     <p>${this.name}</p>
     <p>${this.message}</p>
     <p>${this.count}</p>
-
-    <pre class="pre-scrollable">
-    <ul id="messageslist">
-    ${this.messages.map((m) => html`<li><b>Agent ${m.from}</b> says "${m.message}"</li>`)}
-    </ul>
-    </pre>
-
     <button @click=${this.clickHandler}>Test Agent from ${this.name} in lithtml</button>
     `;
+  }
+
+  doSomething(params){
+    console.log(params)
   }
 
   clickHandler(event) {
@@ -77,4 +60,4 @@ class MessagesComponent extends LitElement {
 }
 
 // Register the new element with the browser.
-customElements.define('messages-component', MessagesComponent);
+customElements.define('component-modele', ComponentModele);
--- compagent/arch/README.md
+++
@@ -1,5 +0,0 @@
-https://blog.usejournal.com/web-components-will-replace-your-frontend-framework-3b17a580831c
-
-# Material WebComponent
-https://dannymoerkerke.github.io/material-webcomponents
-https://blog.usejournal.com/web-components-will-replace-your-frontend-framework-3b17a580831c
