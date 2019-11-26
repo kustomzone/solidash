42ef8676dc1ab5fa1a4a5c3ba076c63eab9e97d3
David <scenaristeur@gmail.com>
2019-11-26 22:03:37 +0100

    app collapse

---
+++ compagent-app/components/component-app.js
@@ -0,0 +1,108 @@
+import { LitElement, css,  html } from '../vendor/lit-element/lit-element.min.js';
+//import { LitElement, css,  html } from 'https://cdn.pika.dev/lit-element/^2.2.1';
+import { HelloAgent } from '../agents/HelloAgent.js';
+
+import "./messages-component.js"
+import "./tripledoc-profile.js"
+import "./browser-component.js"
+import "./explorer-component.js"
+import "./editor-component.js"
+import "./login-component.js"
+
+// Extend the LitElement base class
+class ComponentApp extends LitElement {
+
+  static get properties() {
+    return {
+        name: {type: String}
+    };
+  }
+
+  constructor() {
+    super();
+  this.name = "unknown"
+  }
+
+  firstUpdated(changedProperties) {
+    this.agent = new HelloAgent(this.name);
+
+    var coll = this.shadowRoot.querySelectorAll(".collapsible");
+    for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        console.log("click")
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  }
+
+  render() {
+    return html`
+    <style>
+    /* Style the button that is used to open and close the collapsible content */
+    .collapsible {
+      background-color: #eee;
+      color: #444;
+      cursor: pointer;
+      padding: 18px;
+      width: 100%;
+      border: none;
+      text-align: left;
+      outline: none;
+      font-size: 15px;
+    }
+
+    /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+    .active, .collapsible:hover {
+      background-color: #ccc;
+    }
+
+    /* Style the collapsible content. Note: hidden by default */
+    .content {
+      padding: 0 18px;
+      display: none;
+      overflow: hidden;
+      background-color: #f1f1f1;
+    }
+    </style>
+    <button type="button" class="collapsible"><h1>Login</h1></button>
+    <div class="content">
+      <login-component name="Login"></login-component>
+    </div>
+
+    <button type="button" class="collapsible"><h2>Browser</h2></button>
+    <div class="content">
+      <browser-component name="Browser"></browser-component>
+    </div>
+
+    <button type="button" class="collapsible"><h1>Profile</h1></button>
+    <div class="content">
+    <tripledoc-profile name="TripledocProfile"></tripledoc-profile>
+    </div>
+
+    <button type="button" class="collapsible"><h2>Explorer</h2></button>
+    <div class="content">
+    <explorer-component name="Explorer"></explorer-component>
+    </div>
+
+    <button type="button" class="collapsible"><h1>Editor</h1></button>
+    <div class="content">
+    <editor-component name="Editor"></editor-component>
+    </div>
+
+    <button type="button" class="collapsible"><h2>Messages</h2></button>
+    <div class="content">
+    <messages-component name="Messages"></messages-component>
+    </div>
+    `;
+  }
+
+
+}
+// Register the new element with the browser.
+customElements.define('component-app', ComponentApp);
---
+++ compagent-app/components/explorer-component.js
@@ -0,0 +1,125 @@
+import { LitElement, css,  html } from '../vendor/lit-element/lit-element.min.js';
+//import { LitElement, css,  html } from 'https://cdn.pika.dev/lit-element/^2.2.1';
+import { HelloAgent } from '../agents/HelloAgent.js';
+
+import { SolidFileHelper } from '../helpers/solid-file-helper.js';
+
+// Extend the LitElement base class
+class ExplorerComponent extends LitElement {
+
+  static get properties() {
+    return {
+      name: {type: String},
+      uri: {type: String},
+      folder: { type: Object},
+      erreur: {type : String}
+    };
+  }
+
+  constructor() {
+    super();
+    this.name = "unknown"
+    this.uri = ""
+    this.erreur = ""
+    this.folder = {folders: [], files: []}
+    this.sfh = new SolidFileHelper()
+    //  console.log("SFH",this.sfh)
+  }
+
+  firstUpdated(changedProperties) {
+    var app = this;
+    this.agent = new HelloAgent(this.name);
+    this.agent.receive = function(from, message) {
+      if (message.hasOwnProperty("action")){
+        switch(message.action) {
+          case "exploreFolder":
+          // code block
+          app.exploreFolder(message.uri)
+          break;
+          default:
+          // code block
+          console.log("Unknown action ",message)
+        }
+      }
+    };
+  }
+
+  render() {
+
+    const folderList = (folder) => html`
+
+    ${folder.folders != undefined ? html`<p>something</p>` : html`<p>not something</p>`}
+
+
+    folders (${folder.folders.length})<br>
+    <ul>
+    <li>
+    <button @click=${this.clickFolder} uri=${folder.parent} >.. (${folder.parent})</button>
+    </li>
+    ${folder.folders.map((f) => html`
+      <li>
+      <button @click=${this.clickFolder} uri=${f.url} >${f.name}</button>
+      </li>
+      `)}
+      </ul>
+      `;
+
+
+      const fileList = (files) => html`
+      files (${files.length})<br>
+      <ul>
+      ${files.map((f) => html`
+        <li>
+        <button @click=${this.clickFile} uri=${f.url} >${f.name}</button>
+        </li>
+        `)}
+        </ul>
+        `;
+
+
+
+        return html`
+        <h1>${this.name}</h1>
+        <p> exploration de ${this.uri}</p>
+        <p>Erreur : ${this.erreur}</p>
+        ${folderList(this.folder)}
+        ${fileList(this.folder.files)}
+        `;
+      }
+
+      exploreFolder(uri){
+        var app = this
+        app.erreur = ""
+        this.uri = uri
+        console.log(this.uri)
+        this.sfh.readFolder(this.uri)
+        .then(
+          folder => {
+            if (folder.url != undefined){
+              app.folder = folder
+              console.log("FOLDER",app.folder)
+            }else{
+              app.erreur = folder
+            }
+          }, err => {
+            console.log(err)
+          })
+
+        }
+
+        clickFolder(event) {
+          var uri = event.target.getAttribute("uri");
+          this.exploreFolder(uri)
+        }
+
+        clickFile(event) {
+          var uri = event.target.getAttribute("uri");
+          var message = {action:"fileUriChanged", uri:uri}
+          this.agent.send('Editor', message);
+        }
+
+
+      }
+
+      // Register the new element with the browser.
+      customElements.define('explorer-component', ExplorerComponent);
